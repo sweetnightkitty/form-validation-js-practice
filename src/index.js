0e2306a09ea5a26bf95111e559c9b25862ab4205
SaraToth <saratoth101@gmail.com>
2024-09-16 20:08:46 +0900

    Create global constants for form and form inputs, and add event listener for form submit, and form inputs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,47 @@
 import "./styles.css";
 
+const form = document.querySelector("form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const country = document.getElementById("country");
+const zip = document.getElementById("zip");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+
+form.addEventListener("submit", () => {
+    //Validate form
+})
+
+firstName.addEventListener("change", () => {
+    //validate firstName
+})
+
+lastName.addEventListener("change", () => {
+    //valide last name
+})
+
+email.addEventListener("change", () => {
+    //validate email
+})
+
+phone.addEventListener("change", () => {
+    //validate phone
+})
+
+country.addEventListener("change", () => {
+    //validate country
+})
+
+zip.addEventListener("change", () => {
+    //validate zip code per country
+})
+
+password.addEventListener("change", () => {
+    //validate password
+})
+
+confirmPassword.addEventListener("change", () => {
+    //validate second password
+})
