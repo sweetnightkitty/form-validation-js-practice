cca0b75aac53b6c89c187ced7c2d1a311382dfe2
SaraToth <saratoth101@gmail.com>
2024-09-17 14:40:33 +0900

    Migrate all individual functions that validate one particular input into the form-validation module, and leave validateForm function in index to handle all of the validations in one go

--- src/form-validation.js
+++ src/form-validation.js
@@ -1,42 +1,115 @@
-// import { inputs } from "./index.js";
+export function validateFirstName(firstName) {
+    if(firstName.validity.valueMissing) {
+        const message = "Please provide your first name";
+        setErrorMessage(firstName, message);
+    }
+}
+
+export function validateLastName(lastName) {
+    if(lastName.validity.valueMissing) {
+        const message = "Please provide your last name";
+        setErrorMessage(lastName, message);
+    }
+}
+
+export function validateEmail(email) {
+    if(email.validity.valueMissing) {
+        const message = "Please provide your email address";
+        setErrorMessage(email, message);
+    } else if(email.validity.typeMismatch) {
+        const message = "Please provide a valid email address";
+        setErrorMessage(email, message);
+    }
+}
+
+export function validatePhone(phone) {
+    if(phone.validity.valueMissing) {
+        const message = "Please provide your phone number";
+        setErrorMessage(phone, message);
+    } else if(phone.validity.patternMismatch) {
+        const message = "Please provide a valid 10 digit phone number without any spaces or dashes";
+        setErrorMessage(phone, message);
+    }
+}
+
+export function validateCountry(country) {
+    if(country.validity.valueMissing) {
+        const message = "Please choose your country of residence";
+        setErrorMessage(country, message);
+    }
+}
+
+export function validateZip(country, zip) {
+    const zipCodeFormats = {
+        ch: [
+          "^(CH-)?\\d{4}$",
+          "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+        ],
+        fr: [
+          "^(F-)?\\d{5}$",
+          "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+        ],
+        de: [
+          "^(D-)?\\d{5}$",
+          "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
+        ],
+        nl: [
+          "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+          "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+        ],
+        us: [
+            "^\\d{5}",
+            "US ZIPs must have exactly 5 digits: e.g. 44212",
+        ],
+    };
+
+    if(country.validity.valueMissing) {
+        const message = "Please select a country first";
+        setErrorMessage(zip, message);
+    } else if(zip.validity.valueMissing) {
+        const message = "Please provide your zip code";
+        setErrorMessage(zip, message);
+    } else {
+        const selectedCountry = country.value;
+        const pattern =  zipCodeFormats[selectedCountry][0];
+        const message = zipCodeFormats[selectedCountry][1];
+
+        zip.setAttribute("pattern", pattern);
+        if(zip.validity.patternMismatch) {
+            setErrorMessage(zip, message);
+        }
+    }
+}
+
+export function validatePassword(password) {
+    if(password.validity.valueMissing) {
+        const message = "Please choose a password";
+        setErrorMessage(password, message);
+    } else if(password.validity.tooShort) {
+        const message = "Password must be at least 8 characters long";
+        setErrorMessage(password, message);
+    }
+}
+
+export function validateConfirmPassword(password, confirmPassword) {
+    if(confirmPassword.validity.valueMissing) {
+        const message = "Please type your password again";
+        setErrorMessage(confirmPassword, message);
+    } else if(password.value != confirmPassword.value) {
+        const message = "Passwords do not match";
+        setErrorMessage(confirmPassword, message);
+    }
+}
 
 
+//Intakes the selected input, and returns the error div for it
+function getErrorDiv(input) {
+    const inputController = input.parentElement;
+    const errorDiv = inputController.querySelector(".error");
 
-// export function validateEmptyInputs() {
-//     for(let i = 0; i < inputs.length; i++) {
-//         if(inputs[i].validity.valueMissing) {
-//             //setError on inputs[i]
-//             setErrorMessage(inputs[i]);
-//         }
-//     }
-// }
+    return errorDiv;
+}
 
-// function setErrorMessage(value) {
-//     const inputController = value.parentElement;
-//     const errorDiv = inputController.querySelector(".error");
-//     errorDiv.textContent = "error bro";
-// }
-
-
-// function validateForm() {
-//     const firstName = document.getElementById("first-name");
-//     const lastName = document.getElementById("last-name");
-//     const email = document.getElementById("email");
-//     const phone = document.getElementById("phone");
-//     const country = document.getElementById("country");
-//     const zip = document.getElementById("zip");
-//     const password = document.getElementById("password");
-//     const confirmPassword = document.getElementById("confirm-password");
-
-//     validateName();
-//     validateEmail();
-//     validatePhone();
-//     validateCountry();
-//     validateZip();
-// }
-
-// function validateName(name) {
-//     if(name.validity.valueMissing) {
-//         return 
-//     }
-// }
+function setErrorMessage(input, message) {
+    getErrorDiv(input).textContent = message;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-
 import "./styles.css";
+import { validateFirstName } from "./form-validation";
+import { validateLastName } from "./form-validation";
+import { validateEmail } from "./form-validation";
+import { validatePhone } from "./form-validation";
+import { validateCountry } from "./form-validation";
+import { validateZip } from "./form-validation";
+import { validatePassword } from "./form-validation";
+import { validateConfirmPassword } from "./form-validation";
 
 
 const form = document.querySelector("form");
@@ -18,6 +25,7 @@ form.addEventListener("submit", (e) => {
     validateForm();
 })
 
+//How to fire when tab away empty? - same logic for others
 firstName.addEventListener("change", () => {
     //validate firstName
 })
@@ -39,7 +47,7 @@ country.addEventListener("change", () => {
 })
 
 zip.addEventListener("change", () => {
-    //validate zip code per country
+    validateZip(country, zip);
 })
 
 password.addEventListener("change", () => {
@@ -52,136 +60,12 @@ confirmPassword.addEventListener("change", () => {
 
 
 function validateForm() {
-    validateFirstName();
-    validateLastName();
-    validateEmail();
-    validatePhone();
-    validateCountry();
-    validateZip();
-    validatePassword();
-    validateConfirmPassword();
-}
-
-
-function validateFirstName() {
-    if(firstName.validity.valueMissing) {
-        const message = "Please provide your first name";
-        setErrorMessage(firstName, message);
-    }
-}
-
-function validateLastName() {
-    if(lastName.validity.valueMissing) {
-        const message = "Please provide your last name";
-        setErrorMessage(lastName, message);
-    }
-}
-
-function validateEmail() {
-    if(email.validity.valueMissing) {
-        const message = "Please provide your email address";
-        setErrorMessage(email, message);
-    } else if(email.validity.typeMismatch) {
-        const message = "Please provide a valid email address";
-        setErrorMessage(email, message);
-    }
-}
-
-function validatePhone() {
-    if(phone.validity.valueMissing) {
-        const message = "Please provide your phone number";
-        setErrorMessage(phone, message);
-    } else if(phone.validity.patternMismatch) {
-        const message = "Please provide a valid 10 digit phone number without any spaces or dashes";
-        setErrorMessage(phone, message);
-    }
-}
-
-function validateCountry() {
-    if(country.validity.valueMissing) {
-        const message = "Please choose your country of residence";
-        setErrorMessage(country, message);
-    }
-}
-
-function validateZip() {
-    const zipCodeFormats = {
-        ch: [
-          "^(CH-)?\\d{4}$",
-          "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
-        ],
-        fr: [
-          "^(F-)?\\d{5}$",
-          "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
-        ],
-        de: [
-          "^(D-)?\\d{5}$",
-          "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
-        ],
-        nl: [
-          "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
-          "Netherland ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
-        ],
-        us: [
-            "^\\d{5}",
-            "US ZIPs must have exactly 5 digits: e.g. 44212",
-        ],
-    };
-
-    if(country.validity.valueMissing) {
-        const message = "Please select a country first";
-        setErrorMessage(zip, message);
-    } else if(zip.validity.valueMissing) {
-        const message = "Please provide your zip code";
-        setErrorMessage(zip, message);
-    } else {
-        const selectedCountry = getCountry();
-        const pattern =  zipCodeFormats[selectedCountry][0];
-        const message = zipCodeFormats[selectedCountry][1];
-
-        zip.setAttribute("pattern", pattern);
-        if(zip.validity.patternMismatch) {
-            setErrorMessage(zip, message);
-        }
-    }
-}
-
-function validatePassword() {
-    if(password.validity.valueMissing) {
-        const message = "Please choose a password";
-        setErrorMessage(password, message);
-    } else if(password.validity.tooShort) {
-        const message = "Password must be at least 8 characters long";
-        setErrorMessage(password, message);
-    }
-}
-
-function validateConfirmPassword() {
-    if(confirmPassword.validity.valueMissing) {
-        const message = "Please type your password again";
-        setErrorMessage(confirmPassword, message);
-    } else if(password.value != confirmPassword.value) {
-        const message = "Passwords do not match";
-        setErrorMessage(confirmPassword, message);
-    }
-}
-
-function getCountry() {
-    return country.value;
-}
-
-
-
-
-
-//Intakes the selected input, and returns the error div for it
-function getErrorDiv(input) {
-    const inputController = input.parentElement;
-    const errorDiv = inputController.querySelector(".error");
-
-    return errorDiv;
-}
-
-function setErrorMessage(input, message) {
-    getErrorDiv(input).textContent = message;
+    validateFirstName(firstName);
+    validateLastName(lastName);
+    validateEmail(email); 
+    validatePhone(phone);
+    validateCountry(country);
+    validateZip(country, zip);
+    validatePassword(password);
+    validateConfirmPassword(password, confirmPassword); 
 }
